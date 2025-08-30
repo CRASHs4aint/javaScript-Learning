21dc382326684f30be07a5b8dc618095c51ae600
CRASHs4aint <samratkumarsahu228@gmail.com>
2025-08-30 22:30:51 +0530

    ch-06 & 07 (toggle Btn)

--- ch06/script.js
+++ ch06/script.js
@@ -5,3 +5,24 @@ let boxes=document.querySelectorAll(".box");
 boxes[0].innerText ="I am the first box";
 boxes[1].innerText ="I am the second box";
 boxes[2].innerText ="I am the third box";
+
+const graph=document.getElementById("graphs");
+graph.addEventListener("click",() =>{
+    graph.classList.toggle("font");
+});
+
+
+const body=document.body;
+const tooglebtn =document.getElementById("togglebtn");
+
+tooglebtn.addEventListener("click",() =>{
+body.classList.toggle("dark");
+body.classList.toggle("light");
+
+if(body.classList.contains("dark")){
+    tooglebtn.textContent ="Light Mode";
+   
+}else{
+     tooglebtn.textContent ="dark Mode";
+}
+});
